2629eef94ae596af3dfee1d5f92091e00dfd6c51
Rabin R <suriyarabin@gmail.com>
2026-09-26 22:10:30 +0530

    refactor: remove input and select control components and their associated types

--- apps/admin/src/app/shared/components/input-control/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './input-control.component';
-export * from './input-control.types';
--- apps/admin/src/app/shared/components/select-control/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './select-control.component';
-export * from './select-control.types';
